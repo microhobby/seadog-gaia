9c993797b684cdd9ff039e2449f46f98749901c3
Matheus Castello <matheus@castello.eng.br>
2020-08-02 02:26:30 -0300

    uboot: rpi: Add the build script for Raspberry Pi 3B +
    arm64v8
    
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

---
+++ uboot/rpi/build-raspi-3bp.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export path=""
+export path_boot=""
+export path_modules=""
+export artifacts="./rpi/artifacts/bcm2837-rpi-3bp"
+export defconfig="./rpi/configs/bcm2837_rpi_3bp_defconfig"
+export dtb_prefix="broadcom/bcm28"
+export uboot_src="../u-boot"
+
+./uboot/build-aarch-common.sh "bcm2837-rpi-3bp" "CLEAN"
