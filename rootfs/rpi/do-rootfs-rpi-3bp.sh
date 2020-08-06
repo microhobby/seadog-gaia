767dd6ef99a8e31d6221d072b809625947ee43b6
Matheus Castello <matheus@castello.eng.br>
2020-08-06 20:21:51 -0300

    rootfs: rpi: Use the doRootFs from common instead of call the functions
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- rootfs/rpi/do-rootfs-rpi-3bp.sh
+++ rootfs/rpi/do-rootfs-rpi-3bp.sh
@@ -9,13 +9,4 @@ export arch='arm64'
 export vendor='broadcom'
 export dtb='bcm2837-rpi-3-b-plus.dtb'
 
-prepare
-# create the img file and mount
-createImg $hardware
-mountImg $hardware
-# install boot files and kernel
-doBootfs $family $hardware $arch $vendor $dtb
-# install base distro
-doRootfsArm64 $hardware
-umountImg $hardware
-finish $hardware
+doRootFs
