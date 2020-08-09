e0c09271cee3b36f4627153877e5f1088b7aa9a5
Matheus Castello <matheus@castello.eng.br>
2020-08-09 14:43:15 -0300

    kernel: rpi: bcm2837-rpi-3bp: Fix the dtb_prefix
    In cases that we want to build the downstream Kernel
    
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- kernel/rpi/build-raspi-3bp.sh
+++ kernel/rpi/build-raspi-3bp.sh
@@ -5,7 +5,7 @@ export path_boot=""
 export path_modules=""
 export artifacts="./rpi/artifacts/bcm2837-rpi-3bp"
 export defconfig="./rpi/configs/bcm2837_rpi_3bp_defconfig"
-export dtb_prefix="broadcom/bcm28"
+export dtb_prefix="broadcom/bcm"
 export kernel_src="../linux"
 
 pwd
