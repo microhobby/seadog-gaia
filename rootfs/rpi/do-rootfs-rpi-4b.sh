dbb2f64039a41550f4bb4887178c2c357ef0d908
Matheus Castello <matheus@castello.eng.br>
2020-08-23 12:27:08 -0300

    rootfs: rpi: Add support for Raspberry Pi 4 B
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

---
+++ rootfs/rpi/do-rootfs-rpi-4b.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# use the common functions
+. ./rootfs/do-rootfs-common.sh --source-only
+
+export artifacts="./rpi/artifacts/bcm2711-rpi-4b"
+export kernel_src="../linux"
+export hardware='bcm2711-rpi-4b'
+export family='rpi'
+export arch='arm64'
+export vendor='broadcom'
+export dtb='bcm2711-rpi-4-b.dtb'
+
+doRootFs
