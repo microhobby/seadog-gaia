b0213075e65641eb219b075ac5f42c0d32401818
Matheus Castello <matheus@castello.eng.br>
2021-12-09 14:55:34 -0300

    rootfs: rpi: Add do-rootfs for rpi 3b
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

---
+++ rootfs/rpi/do-rootfs-rpi-3b.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# use the common functions
+. ./rootfs/do-rootfs-common.sh --source-only
+
+export artifacts="./rpi/artifacts/bcm2837-rpi-3b"
+export kernel_src="../linux"
+export hardware='bcm2837-rpi-3b'
+export family='rpi'
+export arch='arm64'
+export vendor='broadcom'
+export dtb='bcm2837-rpi-3-b.dtb'
+
+doRootFs
